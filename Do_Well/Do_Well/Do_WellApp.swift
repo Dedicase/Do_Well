34b757e8cd1f13f42fbcb66ecd3f9aa1b4b3b1f6
Diademe Porzio <107896629+Dedicase@users.noreply.github.com>
2024-05-30 14:14:41 -0600

    Add files via upload

---
+++ Do_Well/Do_Well/Do_WellApp.swift
@@ -0,0 +1,17 @@
+//
+//  Do_WellApp.swift
+//  Do_Well
+//
+//  Created by Diademe Porzio on 5/30/24.
+//
+
+import SwiftUI
+
+@main
+struct Do_WellApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
